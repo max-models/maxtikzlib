e8a13c9385de4bf73dfc73639587c65537bb8f43
maxlin-ipp <max.lindqvist@ipp.mpg.de>
2025-01-27 12:50:55 +0100

    Formatting

--- src/maxtikzlib/console/main.py
+++ src/maxtikzlib/console/main.py
@@ -1,5 +1,6 @@
 from maxtikzlib.figure import TikzFigure
 
+
 def main():
 
     tikz = TikzFigure()
@@ -9,18 +10,26 @@ def main():
     # M
     nodes = [[0, 0], [0, 3], [1, 2], [2, 3], [2, 0]]
     for i, node_data in enumerate(nodes):
-        tikz.add_node(node_data[0], node_data[1], f"M{i}", layer=0, color='red', content=f"Node {i}")
+        tikz.add_node(
+            node_data[0],
+            node_data[1],
+            f"M{i}",
+            layer=0,
+            color="red",
+            content=f"Node {i}",
+        )
     tikz.add_path(
         [f"M{i}" for i in range(len(nodes))], path_actions=path_actions, layer=1
     )
 
     print(tikz.generate_tikz())
 
-    tikz_2 = TikzFigure(tikz_code = tikz.generate_tikz())
+    tikz_2 = TikzFigure(tikz_code=tikz.generate_tikz())
 
     print(tikz_2.generate_tikz())
     # with open('_standalone.tex','w') as f:
     #     f.write(tikz_2.generate_standalone())
+
 
 if __name__ == "__main__":
     main()
