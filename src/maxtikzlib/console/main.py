4825afbe76048ab3652f8157eb4d83a0ec57a6ea
maxlin-ipp <max.lindqvist@ipp.mpg.de>
2025-01-27 15:50:10 +0100

    Removed the testing from main

--- src/maxtikzlib/console/main.py
+++ src/maxtikzlib/console/main.py
@@ -2,33 +2,7 @@ from maxtikzlib.figure import TikzFigure
 
 
 def main():
-
-    tikz = TikzFigure()
-
-    path_actions = ["draw", "rounded corners", "line width=3"]
-
-    # M
-    nodes = [[0, 0], [0, 3], [1, 2], [2, 3], [2, 0]]
-    for i, node_data in enumerate(nodes):
-        tikz.add_node(
-            node_data[0],
-            node_data[1],
-            f"M{i}",
-            layer=0,
-            color="red",
-            content=f"Node {i}",
-        )
-    tikz.add_path(
-        [f"M{i}" for i in range(len(nodes))], path_actions=path_actions, layer=1
-    )
-
-    print(tikz.generate_tikz())
-
-    tikz_2 = TikzFigure(tikz_code=tikz.generate_tikz())
-
-    print(tikz_2.generate_tikz())
-    # with open('_standalone.tex','w') as f:
-    #     f.write(tikz_2.generate_standalone())
+    print("Welcome to MaxTikzLib!")
 
 
 if __name__ == "__main__":
